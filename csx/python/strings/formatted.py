08f02e57b10840f08173fca0d4bf3e02af6a5f58
mhsatman@gmail.com <mhsatman@gmail.com>
2025-02-17 20:29:12 +0300

    add formatted strings in Python section

---
+++ csx/python/strings/formatted.py
@@ -0,0 +1,28 @@
+# Pi
+pi = 3.14159265
+
+print("The value of pi is {}".format(pi))
+
+# The output is as follows:
+# The value of pi is 3.14159265
+
+
+
+
+# Another use of formatted strings
+print(f"The value of pi is {pi}")
+
+# The output is as follows:
+# The value of pi is 3.14159265
+
+
+
+
+# Mutliple variables
+name = "John"
+age = 23
+print(f"{name} is {age} years old.")
+
+# Mutliple variables revisited
+print("{} is {} years old.".format(name, age))
+
