47ca102dae1eb4590e55e2df3bdc21ec3d0ac89b
mhsatman@gmail.com <mhsatman@gmail.com>
2025-02-27 20:05:36 +0300

    correct scanfexample.c

--- csx/C/fileio/scanfexample.c
+++ csx/C/fileio/scanfexample.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char c[20];
     
     printf("Enter some text: ");
-    scanf("%s", &c);
+    scanf("%s", c);
     
     printf("You entered: %s\n", c);
-    printf("Length: %d\n", strlen(c));
+    printf("Length: %ld\n", strlen(c));
     return 0;
 }
