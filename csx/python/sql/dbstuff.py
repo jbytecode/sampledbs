f2ab997f07cd40d307c5ffd2beb87ee94b906d5f
mhsatman@gmail.com <mhsatman@gmail.com>
2025-02-28 20:30:04 +0300

    update sqlite example in Python

--- csx/python/sql/dbstuff.py
+++ csx/python/sql/dbstuff.py
@@ -2,23 +2,27 @@ import sqlite3
 
 db = sqlite3.connect("mydatabase.db")
 
-db.execute("create table if not exists Student (id text, name text, address text)")
+cur = db.cursor()
 
-db.execute("delete from Student")
+cur.execute("create table if not exists Student (id text, name text, address text)")
 
-db.execute("insert into Student (id, name, address) values ('1', 'Alice', '123 Main St.')")
-db.execute("insert into Student (id, name, address) values ('2', 'Bob', '456 Elm St.')")
-db.execute("insert into Student (id, name, address) values ('3', 'Charlie', '789 Oak St.')")
+cur.execute("delete from Student")
 
+cur.execute("insert into Student (id, name, address) values ('1', 'Alice', '123 Main St.')")
+cur.execute("insert into Student (id, name, address) values ('2', 'Bob', '456 Elm St.')")
+cur.execute("insert into Student (id, name, address) values ('3', 'Charlie', '789 Oak St.')")
 
-results = db.execute("select * from Student")
+db.commit()
+
+results = cur.execute("select * from Student")
 
 for row in results:
     print(row)
 
-db.execute("update Student set address = '999 Pine St.' where id = '3'")
+cur.execute("update Student set address = '999 Pine St.' where id = '3'")
+db.commit()
 
-results = db.execute("select * from Student")
+results = cur.execute("select * from Student")
 
 for row in results:
     print(row)
